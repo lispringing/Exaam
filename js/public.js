ed0ca06eab1db197af19d61c28528cefe94e5329
lispringing <2638526782@qq.com>
2024-12-05 21:07:59 +0800

    题库更新

--- js/public.js
+++ js/public.js
@@ -1,4 +1,7 @@
 const JSONList = [
     { id: "00001", name: '团校考试-单选题', describe: '团校考试的单选题题库', file: "团校单选.json" },
     { id: "00002", name: '团校考试-判断题', describe: '团校考试的判断题题库', file: "团校判断.json" },
+    { id: "00003", name: '团校考试-多选题', describe: '团校考试的多选题题库', file: "团校多选.json" },
+    { id: "00004", name: '团校考试-论述题', describe: '团校考试的论述题题库', file: "团校论述.json" },
+    { id: "00005", name: '团校考试-简答题', describe: '团校考试的简答题题库', file: "团校简答.json" },
 ]
